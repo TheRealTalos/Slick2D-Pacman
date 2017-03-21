7b9e385bec598b48cdbd2db16588df886aadbb3b
TheRealTalos <Zack@192.168.0.12>
2017-03-20 21:55:34 -0400

    Added Animation, TileMap and Fixed

--- Slick2D-Pacman/src/main/Pacman.java
+++ Slick2D-Pacman/src/main/Pacman.java
@@ -1,12 +1,26 @@
 package main;
 
+import java.awt.Container;
+
 import org.newdawn.slick.*;
 import org.newdawn.slick.tiled.TiledMap;
 
 public class Pacman extends BasicGame{
 	
+	//MAKE SCALABLE GAME
+	//MAKE SPRITESHEET WORK
+	//ADD PLAYER CLASS
+	//MESS WITH SETTINGS
+	
 	private TiledMap map;
-	private Animation pacAnimRight, pacAnimLeft;
+	private Animation pacMan, pacAnimUp, pacAnimDown, pacAnimRight, pacAnimLeft;
+	
+	private static final int WORLDSIZE = 336;
+	private static final int TILESIZE = 16;
+	private static final int SCALE = 2;
+	
+	private float x = 10*TILESIZE;
+	private float y = 15*TILESIZE;
 	
 	public Pacman() {
 		super("Pacman");
@@ -17,11 +31,8 @@ public class Pacman extends BasicGame{
 		try{
 			
 			AppGameContainer app = new AppGameContainer(new Pacman());
-			app.setDisplayMode(960, 540, false);
+			app.setDisplayMode(WORLDSIZE, WORLDSIZE, false);
 			app.start();
-			
-			Image[] pacImageRight = {new Image("res/sprites/right1.png"), new Image("res/sprites/right2.png")};
-			Image[] pacImageLeft = {new Image("res/sprites/left1.png"), new Image("res/sprites/left2.png")};
 			
 		}catch(SlickException e){
 			e.printStackTrace();
@@ -34,23 +45,50 @@ public class Pacman extends BasicGame{
 		
 		map = new TiledMap("res/maps/map.tmx");
 		
-		pacAnimLeft = new Animation(pacImageLeft, 16);
-		pacAnimRight = new Animation(pacImageRight, 16);
+		Image[] pacImageUp = {new Image("res/sprites/PacUp1.png"), new Image("res/sprites/PacUp2.png")};
+		Image[] pacImageDown = {new Image("res/sprites/PacDown1.png"), new Image("res/sprites/PacDown2.png")};
+		Image[] pacImageRight = {new Image("res/sprites/PacRight1.png"), new Image("res/sprites/PacRight2.png")};
+		Image[] pacImageLeft = {new Image("res/sprites/PacLeft1.png"), new Image("res/sprites/PacLeft2.png")};
+		
+		pacAnimUp = new Animation(pacImageUp, 100, false);
+		pacAnimDown = new Animation(pacImageDown, 100, false);
+		pacAnimLeft = new Animation(pacImageLeft, 100, false);
+		pacAnimRight = new Animation(pacImageRight, 100, false);
+		
+		pacMan = pacAnimRight;
 	}
 	
 	@Override
 	public void render(GameContainer arg0, Graphics arg1) throws SlickException {
 		
 		map.render(0, 0);
-		
+		pacMan.draw(x, y);
 		
 	}
 
 	@Override
-	public void update(GameContainer arg0, int arg1) throws SlickException {
+	public void update(GameContainer container, int delta) throws SlickException {
 		
-		
-		
+		Input input = container.getInput();
+		if (input.isKeyDown(Input.KEY_UP)){
+			pacMan = pacAnimUp;
+			pacMan.update(delta);
+			y -= delta * 0.1f;
+			
+		}else if (input.isKeyDown(Input.KEY_DOWN)){
+			pacMan = pacAnimDown;
+			pacMan.update(delta);
+			y += delta * 0.1f;
+			
+		}else if (input.isKeyDown(Input.KEY_LEFT)){
+			pacMan = pacAnimLeft;
+			pacMan.update(delta);
+			x -= delta * 0.1f;
+			
+		}else if (input.isKeyDown(Input.KEY_RIGHT)){
+			pacMan = pacAnimRight;
+			pacMan.update(delta);
+			x += delta * 0.1f;
+		}
 	}
-
 }
